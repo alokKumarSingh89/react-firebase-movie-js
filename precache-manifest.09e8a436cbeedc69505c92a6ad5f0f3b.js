d5833b37ad61a8f643e2f7e81665f37b8249f528
alokKumarSingh89 <itsaloksigh2011@gmail.com>
2020-07-11 18:04:54 +0800

    Updates

---
+++ precache-manifest.09e8a436cbeedc69505c92a6ad5f0f3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ff0a5bdbdadc7977ba2d72ebc11588",
+    "url": "/react-firebase-movie/index.html"
+  },
+  {
+    "revision": "5cdcdfa724554936dd43",
+    "url": "/react-firebase-movie/static/css/main.5cc662a5.chunk.css"
+  },
+  {
+    "revision": "1531ffa1383bb0192ea0",
+    "url": "/react-firebase-movie/static/js/2.fc3d3b21.chunk.js"
+  },
+  {
+    "revision": "5cdcdfa724554936dd43",
+    "url": "/react-firebase-movie/static/js/main.50bdfee9.chunk.js"
+  },
+  {
+    "revision": "71b0482a10ac2fa0f5a7",
+    "url": "/react-firebase-movie/static/js/runtime-main.6d90f992.js"
+  }
+]);
